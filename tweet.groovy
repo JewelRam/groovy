69c97bb9d3dc76b1a406ef21a61205393f9c6469
Jewel Ramirez <chimpy4u@yahoo.com>
2019-02-19 11:05:50 -0800

    adding files

---
+++ tweet.groovy
@@ -0,0 +1,18 @@
+class tweet {
+
+String userName
+String text
+Integer reTweets
+Integer favorties
+Date createdOn
+
+public Tweet(Stirng user, String tweet) {
+reTweets = 0
+favorties = 0
+text = tweet
+createdOn = new Date()
+
+}
+
+
+}
